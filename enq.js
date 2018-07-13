136424f7c86746e637225ba6961f47b027af073e
fukuchi <tokyo@keio.jp>
2018-07-13 10:52:46 +0900

    test query

--- enq.js
+++ enq.js
@@ -137,8 +137,28 @@ function endEvent() {
   }
 }
 
+function function getUrlVars() {
+  var vars = [], max = 0, hash = "", array = "";
+  var url = window.location.search;
+
+  hash  = url.slice(1).split('&');    
+  max = hash.length;
+  for (var i = 0; i < max; i++) {
+    array = hash[i].split('=');    //keyと値に分割。
+    vars.push(array[0]);    //末尾にクエリ文字列のkeyを挿入。
+    vars[array[0]] = array[1];    //先ほど確保したkeyに、値を代入。
+  }
+
+  return vars;
+}
+
 
 $(function(){
+  var val = getUrlVars();
+  if (val["epi"]) {
+    episode_name = [val["epi"]];
+  }
+
 	var header = document.getElementById("header");
 	var main = document.getElementById("main");
 	var answer_bar = document.getElementById("bar");
